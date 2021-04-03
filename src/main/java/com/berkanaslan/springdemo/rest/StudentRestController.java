80556f8733f0604753ffe25098efa799b742efdc
Berkan Aslan <aslnberkan@gmail.com>
2021-04-04 00:34:32 +0300

    Exception added.

--- src/main/java/com/berkanaslan/springdemo/rest/StudentRestController.java
+++ src/main/java/com/berkanaslan/springdemo/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.berkanaslan.springdemo.rest;
 
 import com.berkanaslan.springdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -37,6 +36,10 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if ((studentId >= students.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student not found for " + studentId);
+        }
+
         return students.get(studentId);
     }
 }
